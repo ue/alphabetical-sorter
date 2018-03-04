7601701771e53ed2c5a5b37da84f20599c21ec3e
ue <ugurerdal93@gmail.com>
2018-03-05 00:40:18 +0300

    created extension :bowtie:

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1 align="center"> Alphabetical Sorter - VScode Extension </h1>
+
+<p align="center"> Easy way for sorting alphabetical! 😎</p>
+
+<p align="center"> Visual Studio Code Extension - You can use in multi line or single for sorting.</p>
+<p align="center"><img align="center" src="https://media.giphy.com/media/28evaf0snirPcDCigC/giphy.gif"/></p>
+
+## Install
+
+Click to extension tab and search `Alphabetical Sorter` and install it reload your editor. Or use this [Alphabetical Sorter](https://marketplace.visualstudio.com/items?itemName=ue.alphabetical-sorter).
+
+## Usage
+
+Select the desired ones(supports for kind variable).
+
+`Cmd+Shift+P` and select `Alphabetical Sorter` thats it.
+
+## For questions
+
+[Ugur Erdal](https://github.com/ue)
+[Repo](https://github.com/ue/alphabetical-sorter)
+
+## License
+
+MIT © [UE](https://github.com/ue)
+
+**Keep cool!**
---
+++ extension.js
@@ -0,0 +1,52 @@
+const vscode = require("vscode");
+
+function activate(context) {
+  let disposable = vscode.commands.registerCommand(
+    "extension.sortIt",
+    function() {
+      let editor = vscode.window.activeTextEditor;
+      let lines = [];
+      let selectionLength;
+
+      const selection = editor.selection;
+      const startPoint = selection.start.line;
+      const endPoint = selection.end.line;
+      const isSingleLine = selection.isSingleLine;
+
+      if (isSingleLine) {
+        selectionLength = editor.document.getText(selection).length;
+        lines = editor.document
+          .getText(selection)
+          .replace(/\s/g, "")
+          .split(",");
+      } else {
+        selectionLength = editor.document.lineAt(endPoint).text.length;
+        for (let i = startPoint; i <= endPoint; i++) {
+          lines.push(editor.document.lineAt(i).text);
+        }
+      }
+
+      lines.sort((a, b) => {
+        if (a < b) return -1;
+        else if (a > b) return 1;
+        return 0;
+      });
+
+      editor.edit(editBuilder => {
+        isSingleLine
+          ? editBuilder.replace(selection, lines.join(", "))
+          : editBuilder.replace(
+              new vscode.Range(startPoint, 0, endPoint, selectionLength),
+              lines.join("\n")
+            );
+      });
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+exports.deactivate = deactivate;
